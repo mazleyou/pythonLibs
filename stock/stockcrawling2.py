86d977cb45ee98bbc463aa3d2f4b463e815ef40d
mazleyou <mazle@nate.com>
2020-08-27 15:27:17 +0900

    200827 - hskim

--- stock/stockcrawling2.py
+++ stock/stockcrawling2.py
@@ -1,15 +1,15 @@
-# from datetime import datetime
-# from time import sleep
-# import schedule
-#
-# def job():
-#     today_date = datetime.today().strftime("%Y%m%d")
-#     print(today_date + " I'm working...")
-#
-# schedule.every().day.at("15:05").do(job)
-#
-# while True:
-#     schedule.run_pending()
-#     sleep(100)
+from datetime import datetime
+from time import sleep
+import schedule
+
+def job():
+    today_date = datetime.today().strftime("%Y%m%d")
+    print(today_date + " I'm working...")
+
+schedule.every().day.at("15:40").do(job)
+
+while True:
+    schedule.run_pending()
+    sleep(100)
 
 print('---------------')
