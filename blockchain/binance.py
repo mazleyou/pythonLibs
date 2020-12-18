b044fbc17dc3091d771ffdcfaeb9db2589b635a8
taihoinst01 <bobby404@naver.com>
2020-12-18 16:04:26 +0900

    201218 - binance add - hskim

--- blockchain/binance.py
+++ blockchain/binance.py
@@ -3,9 +3,6 @@ import pymysql
 from apscheduler.schedulers.background import BackgroundScheduler
 import time
 
-import datetime
-
-
 def klines():
     try:
         print("main start")
@@ -18,14 +15,13 @@ def klines():
         url = 'https://api.binance.com/api/v1/klines?symbol=' + market + '&interval=' + tick_interval
         data = requests.get(url).json()
 
-        dt = datetime.date.today().strftime("%Y%m%d")
-
         for row in data:
-            curs.execute("""select * from BTCUSDT_KLINES where date = %s and OPEN_TIME = %s""", (dt, row[0]))
+            opentime = int(row[0]/10000)
+            curs.execute("""select * from BTCUSDT_KLINES where OPEN_TIME = %s""", opentime)
             exist_record = curs.fetchone()
             if exist_record is None:
-                print(row[0])
-                curs.execute("""insert into BTCUSDT_KLINES(DATE, OPEN_TIME, OPEN, HIGH, LOW, CLOSE, VOLUME, QUOTE, TRADES) values (%s, %s, %s, %s, %s, %s, %s, %s, %s)""", (dt, row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]))
+                print(opentime)
+                curs.execute("""insert into BTCUSDT_KLINES(OPEN_TIME, OPEN, HIGH, LOW, CLOSE, VOLUME, TRADES) values (%s, %s, %s, %s, %s, %s, %s)""", (opentime, row[1], row[2], row[3], row[4], row[5], row[7]))
 
     except Exception as e:
         print(e)
@@ -34,8 +30,8 @@ try:
     print("job start")
     sched = BackgroundScheduler()
     sched.start()
-    sched.add_job(klines, 'interval', hours=4)
-    # sched.add_job(main, 'interval', seconds=10)
+    # sched.add_job(klines, 'interval', hours=4)
+    sched.add_job(klines, 'interval', seconds=59)
 except Exception as e:
     print(e)
 while True:
