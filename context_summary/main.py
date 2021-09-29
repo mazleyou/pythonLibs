8670841a8aef7e6325ffda61740ac94700555f97
mazleyou <mazleyou@gmail.com>
2021-09-29 13:45:25 +0900

    210503 - luis programmatic assign add - hskim

--- context_summary/main.py
+++ context_summary/main.py
@@ -23,16 +23,30 @@ def split_sentences(input):
     return ret_sentences
 
 
-def get_match(labels, target):
-    ret_index = 0
-    feature_tokens = word_tokenize(target)
-    for feature_word in feature_tokens:
-        last_check = False
-        if SequenceMatcher(None, feature_word, labels[ret_index]).ratio() >= 0.5:
-            ret_index += 1
-            last_check = True
+def get_match(labels, targets):
+    ret_l = {}
+    ret_f = {}
+    for idx_l, labels in enumerate(labels):
+        max_ratio = 0
+        for idx_f, features in enumerate(targets):
+            now_ratio = SequenceMatcher(None, labels, features).ratio()
+            if now_ratio > max_ratio:
+                if max_ratio > 0.5:
+                    result[idx_l] = [labels, feature_sentences[idx_f - 1] + features, max_ratio]
+                    result_f[features] = 1
+                else:
+                    if idx_l in result:
+                        result_f[result[idx_l][1]] = 0
+                    result[idx_l] = [labels, features, max_ratio]
+                max_ratio = now_ratio
+            else:
+                if now_ratio > 0.5:
+                    result[idx_l] = [labels, result[idx_l][1] + features, max_ratio]
+                    result_f[features] = 1
+                else:
+                    result_f[features] = 0
 
-    return last_check, ret_index
+    return ret_l, ret_f
 
 
 df = pd.read_csv('train_data.csv')
@@ -45,27 +59,8 @@ for i, row in df.iterrows():
         feature_sentences = split_sentences(row['text'])
         # # Label 문장 분리
         label_sentences = split_sentences(row['summary'])
-
         # Feature sentences에서 중요 문장과 비중요 문장 구분
-        results = []
-        result = {}
-
-        label_index = 0
-        label_tokens = word_tokenize(row['summary'])
-
-        for idx_l, labels in enumerate(label_sentences):
-            max_ratio = 0
-            for idx_f, features in  enumerate(feature_sentences):
-                now_ratio = SequenceMatcher(None, labels, features).ratio()
-                if now_ratio > max_ratio:
-                    if max_ratio > 0.5:
-                        result[idx_l] = [labels, feature_sentences[idx_f-1] + features, max_ratio]
-                    else:
-                        result[idx_l] = [labels, features, max_ratio]
-                    max_ratio = now_ratio
-                else:
-                    if now_ratio > 0.5:
-                        result[idx_l] = [labels, result[idx_l][1] + features, max_ratio]
+        result_l, result_f = get_match(label_sentences, feature_sentences)
 
 
         print("s")
